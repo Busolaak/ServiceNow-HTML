4f75d51b780baa094eb74ec11f6ea27c15482865
Busola <busola@hotmail.coom>
2022-02-01 15:27:22 +0000

    new files added, fizzbuzz.html/js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-`use strict`
+`use strict`;
 
 // let a = 2;
 
@@ -11,6 +11,52 @@
 // console.error("taurus");
 
 
-let mytxt = "this is busola";
+// let mytxt = "this is busola";
 
-console.log("This is now %c the end of the exercises. " +mytxt, "color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+// console.log("This is now %c the end of the exercises. " +mytxt, "color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
+
+
+// let a = 1;
+// const b = 2;
+
+// var c = 3;
+
+// let aBoolean = true;
+// console.log(aBoolean);
+
+// let aNumber = 6;
+// console.log(aNumber);
+
+// let aString = 'text';
+// console.log(aString);
+
+// let totalMoney = 4000;
+// let moneyPaidSoFar = 2348;
+// let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+// console.log(`The total bill is £${totalMoney} the remaining amount of money to be paid is £${totalLeftToPay}`);
+
+// let a = 100;
+// while (a <= 200) {
+// a++;
+// console.log(`a = ${a}`);
+// }
+
+// for(let i = 0; i < 10; i++) {
+//   for(let e = 1; e <= 10; e++) {
+//       console.log(e)
+//   }
+//}
+
+let puppies = 0;
+let notEnoughPups = true;
+
+while (notEnoughPups) {
+  console.log("Another pup");
+  puppies++;
+
+  if (puppies > 238) {
+    notEnoughPups = false;
+  }
+}
+console.log("Oh so many puppies!");
