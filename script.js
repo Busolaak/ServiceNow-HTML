067ebd821f082ef8a50c127ba0a129dc69ceae15
Busola <busola@hotmail.coom>
2022-02-01 10:29:00 +0000

    new files added internet.html, script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+`use strict`
+
+// let a = 2;
+
+// console.log("Busola");
+
+// console.info("Ak");
+
+// console.warn("london");
+
+// console.error("taurus");
+
+
+let mytxt = "this is busola";
+
+console.log("This is now %c the end of the exercises. " +mytxt, "color: orange;font-family:fantasy; font-style: Bold; background-color: black;padding: 10px");
