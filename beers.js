e10469ea328f12a98a69074e61c8606a8a838031
Busola <busola@hotmail.coom>
2022-02-01 16:00:55 +0000

    new files added, beer song.html/js

---
+++ beers.js
@@ -0,0 +1,20 @@
+`use script`
+
+function beerSong() {
+var bottles;
+var bottlesLeft;
+for (i = 99; i >= 3; i--) {
+if (i == 1) { 
+bottles = "bottle";
+bottlesLeft = "No bottles of beer on the wall";
+} else {
+bottles = "bottles";
+bottlesLeft = i - 1 + " bottles of beer on the wall!";
+} console.log(i+ " " + bottles + " of beer on the wall,");
+console.log(i+ " " + bottles + " of beer,");
+console.log("Take one down, pass it around,");
+console.log(bottlesLeft);
+    }
+}
+
+console.log(beerSong());
