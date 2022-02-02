f1f8380ab204d66e6e0ebceb2b7cf93f2d33b15e
Busola <busola@hotmail.coom>
2022-02-02 12:44:18 +0000

    new files added array.html/js

---
+++ arrays.js
@@ -0,0 +1,32 @@
+// arrays, objects and json practice
+
+
+let darthVader = {
+allegiance: "Empire",
+ weapon: "lightsaber",
+ sith: true,
+ jedi: false
+ };
+
+ console.log(darthVader);
+
+
+
+ 
+
+
+ // functions 
+
+ function subCalc(num1, num2) {
+    return num1 - num2;
+}
+
+console.log(subCalc(10,1000));
+
+
+const welcome = function(name,age,gender){
+    return console.log(`My name is ${name}, i am ${age} years old and of gender ${gender}`);
+    }
+   
+    welcome("Felix Cited",20,"Male");
+    
